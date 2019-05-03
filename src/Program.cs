2a7de15e0436cd941032f2016950af7427ce0e4e
Kevin Boronka <kboronka@gmail.com>
2019-05-02 20:27:12 -0400

    added new project

---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using HttpPack.Server;
+
+namespace das_build_agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "4700");
+            var secret = Environment.GetEnvironmentVariable("SECRET") ?? "secret-goes-here";
+            var sandbox = Environment.GetEnvironmentVariable("SANDBOX_PATH") ?? @"c:\temp";
+
+            var server = new HttpServer(port, null, null);
+
+            Console.WriteLine("Press a key to stop");
+            Console.ReadKey();
+        }
+    }
+}
