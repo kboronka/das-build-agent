711954d6f49a40b5f2d16dc2de077c96f52696e1
Kevin Boronka <kboronka@gmail.com>
2019-05-02 21:46:01 -0400

    added sample action to controller

--- src/Controllers/TaskController.cs
+++ src/Controllers/TaskController.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 
+using HttpPack.Json;
+using HttpPack.Server;
+
 namespace DasBuildAgent.Controllers
 {
+    [IsPrimaryController]
+    [IsController]
     class TaskController
     {
+        [IsPrimaryAction]
+        public static HttpContent Index(HttpRequest request)
+        {
+            var sample = new JsonKeyValuePairs();
+            sample.Add("test", "ok");
+
+            return new HttpJsonContent(sample);
+        }
     }
 }
